85cb3b811f2bfac52055be40ff770cf32b8d9385
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2021-12-05 01:39:45 +0100

    updated mechanic

--- sample/src/index.js
+++ sample/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<React.StrictMode>
+	<App />
+</React.StrictMode>, document.getElementById('root'));
 
+serviceWorkerRegistration.register();
 reportWebVitals();
