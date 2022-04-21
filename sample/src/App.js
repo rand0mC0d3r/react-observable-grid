828f955cfdfe1824c3a49284e9fb979ec47e4fba
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-04-21 20:17:21 +0200

    updated UI

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,8 +1,8 @@
 import { Avatar, Button, Chip, Grid as Flexbox, Typography } from '@material-ui/core';
 import { createTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import { useMemo, useState } from 'react';
-import { Grid, GridColumns, GridHeaders, GridRows, GridStats, GridSticky } from 'react-observable-grid';
-// import { Grid, GridColumns, GridHeaders, GridRows, GridStats, GridSticky } from './components';
+// import { Grid, GridColumns, GridHeaders, GridRows, GridStats, GridSticky } from 'react-observable-grid';
+import { Grid, GridColumns, GridHeaders, GridRows, GridStats, GridSticky } from './components';
 // import { Grid } from './components/GridStoreNg';
 import ColumnManager from './parts/ColumnManager';
 import DataStores from './parts/DataStores';
