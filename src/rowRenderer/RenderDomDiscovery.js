8a1f56986f0cc1c80c41cf14c30ebda606ee755d
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-05-21 13:50:47 +0200

    updated UI

--- src/rowRenderer/RenderDomDiscovery.js
+++ src/rowRenderer/RenderDomDiscovery.js
@@ -1,11 +1,16 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import GridObservable from '../GridObservable';
 import DataProvider from '../GridStore';
 
-export default ({ presentColumns, _visibleIndexes }) => {
-  const { uniqueId, data, gridTemplateColumns, global, averageHeight, initialHeight } = useContext(DataProvider)
+export default ({ presentColumns }) => {
+  const { uniqueId, data, gridTemplateColumns, global } = useContext(DataProvider)
 
-  const renderDOMWithDiscoveryItem = ({signature, dataItem}) => <>
+  const signature = (dataItem) => {
+    console.log(dataItem)
+    return Object.values(dataItem).filter(d => typeof d === 'string').join("")
+  }
+
+  const renderDOMWithDiscoveryItem = ({ signature, dataItem }) => <>
     {presentColumns.map(({ key }) => <div
       key={`${signature}.${key}`}
       className={`${uniqueId}-row-discovered`}
@@ -16,27 +21,20 @@ export default ({ presentColumns, _visibleIndexes }) => {
     </div>)}
   </>
 
-  const renderDOMWithDiscovery = () => {
-    const signature = (data) => Object.values(data).filter(d => typeof d === 'string').join("")
-    return <>
-      {!!data?.length && data.map((dataItem, index) => <GridObservable
-        {...{ index }}
-        id={`${uniqueId}.${index}`}
-        key={signature(dataItem)}
-        inViewClassName={`${uniqueId}-row-discovered-wrapper`}
-        style={{
-          backgroundColor: global?.alternatingRows?.stepping(index)
-            ? global?.alternatingRows?.color
-            : index % 2 === 0 ? '#f0f0f0' : 'transparent',
-          padding: global?.style?.rowPadding || '0',
-          gap: `${parseInt(global?.style?.gap?.replace('px', '')) || 0}px`,
-          gridTemplateColumns,
-        }}
-      >
-        {renderDOMWithDiscoveryItem({ signature: signature(data), dataItem })}
-      </GridObservable>)}
-    </>
-  }
-
-  return renderDOMWithDiscovery()
+  return <>{!!data?.length && data.map((dataItem, index) => <GridObservable
+    {...{ index }}
+    id={`${uniqueId}.${index}`}
+    key={signature(dataItem)}
+    inViewClassName={`${uniqueId}-row-discovered-wrapper`}
+    style={{
+      backgroundColor: global?.alternatingRows?.stepping(index)
+        ? global?.alternatingRows?.color
+        : index % 2 === 0 ? '#f0f0f0' : 'transparent',
+      padding: global?.style?.rowPadding || '0',
+      gap: `${parseInt(global?.style?.gap?.replace('px', '')) || 0}px`,
+      gridTemplateColumns,
+    }}
+  >
+    {renderDOMWithDiscoveryItem({ signature: signature(data), dataItem })}
+  </GridObservable>)}</>
 }
